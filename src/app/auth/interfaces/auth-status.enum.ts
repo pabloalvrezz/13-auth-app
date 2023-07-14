59c1b41f68e686f55d8182035ad12a58f600ef6f
Pablo <pabloag43@educastur.es>
2023-07-14 13:49:28 +0200

    creamos las interfaces

---
+++ src/app/auth/interfaces/auth-status.enum.ts
@@ -0,0 +1,5 @@
+export enum AuthStatus {
+  auntenticated = 'auntenticated',
+  checking = 'checking',
+  notAuntenticated = 'notAuntenticated',
+}
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { AuthStatus, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject(HttpClient);
+
+  private _curretnUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
   constructor() { }
+
+  // metodo que usaremos para loggear al usuario
+  login(email: string, password: string): Observable<boolean> {
+
+    return of(true)
+  }
 }
