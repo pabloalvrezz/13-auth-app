6de1a2e7812e85abad77422fbfc8c758f9ac4f22
Pablo <pabloag43@educastur.es>
2023-07-14 13:18:26 +0200

    Creamos el register page

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent {
+  private fb = inject(FormBuilder);
 
+  public myForm: FormGroup = this.fb.group(
+    {
+      name: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(25)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
+  // metodo que usara el boton de registrar y registrará al usuario cuando el formulario sea valido
+  public register() {
+    console.log(this.myForm.value);
+
+    this.myForm.reset()
+  }
 }
